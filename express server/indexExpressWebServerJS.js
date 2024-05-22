88401e3a987398243bda4812e08e0a4ce7840b65
Yellik <yelenakensborn@gmail.com>
2024-05-22 17:55:32 +0200

    added a json response

---
+++ express server/indexExpressWebServerJS.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const morgan = require('morgan');
+const fs = require('fs');
+const { log } = require('util');
+
+const app = express()
+
+app.use(morgan('tiny'));
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+    res.send('Hello fellow developer')
+
+const fileNameOfUrl = (url) => {
+    let fileName = '';
+    if(req.url.split('/')[1] === '') {
+        fileName = 'index.html'
+    }else{
+        fileName = req.url.split('/')
+    }
+    return fileName
+}
+
+const fileName = fileNameOfUrl(req.url);
+
+
+const fileContentOr404 = (fileName) => {
+    if(!fs.existsSync(`static/${fileName}`)){
+        fileName = '404.html';
+    }
+
+    return fs.readFileSync(`static/${fileName}`)
+}
+const content = fileContentOr404(fileName)
+res.end(content)
+});
+
+const port = 3000
+app.listen((port), () => {
+    console.log(`server is running on port ${port}`);
+})
--- express server/server.js
+++ express server/server.js
@@ -1,40 +1,18 @@
 const express = require('express');
-const morgan = require('morgan');
-const fs = require('fs');
 
-const app = express()
-
-app.use(morgan('tiny'));
-app.use(express.static('static'));
+const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello fellow developer')
-
-
-const fileNameOfUrl = url => {
-    let fileName = '';
-    if(req.url.split('/')[1] === '') {
-        fileName = 'index.html'
-    }else{
-        fileName = req.url.split('/')
+    const dev = {
+        id: 1,
+        name: "Jelena",
+        email: "yelli@gmail"
     }
-    return fileName
-}
-
-const fileName = fileNameOfUrl(req.url);
-
-const fileContentOr404 = (fileName) => {
-    if(!fs.existsSync(`static/${fileName}`)){
-        fileName = '404.html';
-    }
-
-    return fs.readFileSync(`static/${fileName}`)
-}
-const content = fileContentOr404(fileName)
-res.end(content)
+    res.json(dev)
 });
 
-const port = 3000
-app.listen((port), () => {
-    console.log(`server is running on port ${port}`);
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`app is listening to the port ${port}`);
 })
