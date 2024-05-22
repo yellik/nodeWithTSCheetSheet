630dbc80d7a08e37c93a1d60a7a89bb38e5bc3e6
Yellik <yelenakensborn@gmail.com>
2024-05-22 18:36:47 +0200

    added dev id loop

--- express server/server.js
+++ express server/server.js
@@ -2,18 +2,38 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    const dev = {
+const db = [
+    {
         id: 1,
         name: "Jelena",
         email: "yelli@gmail"
+    },
+    {
+        id: 2,
+        name: "Linus",
+        email: "linus@gmail.com"
     }
+]
+app.get('/api/developers', (req, res) => {
     res
-    .status(201)
-    .setHeader('location', 'api/developers/1')
-    .json(dev)
+    .status(200)
+    .json(db)
 });
 
+app.get('/api/developers/:id', (req, res) => {
+    console.log(`The db contains ${db.length} objects`);
+    
+    for(let i = 0; i < db.length; i++){
+        console.log(db[i].id);
+        if(db[i].id == req.params.id){
+            console.log('a match is found');
+            res.json(db[i]);
+            return;
+        }
+       
+    }
+    res.status(404).end();
+})
 const port = 3000;
 
 app.listen(port, () => {
