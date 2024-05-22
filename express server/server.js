184e153da9083546a836b29529687ad9d90b1092
Yellik <yelenakensborn@gmail.com>
2024-05-22 16:57:30 +0200

    added a basic express server

---
+++ express server/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+    res.send('Hello fellow developer')
+});
+
+const port = 3000
+app.listen((port), () => {
+    console.log(`server is running on port ${port}`);
+})
