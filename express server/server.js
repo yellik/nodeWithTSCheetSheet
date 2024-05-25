e31434339a9a3ad7e1a514088367e2e921cb2e25
Yellik <yelenakensborn@gmail.com>
2024-05-25 19:51:51 +0200

    added dynamic req para\ms

--- express server/server.js
+++ express server/server.js
@@ -22,7 +22,8 @@ app.get('/api/developers', (req, res) => {
 
 app.get('/api/developers/:id', (req, res) => {
     console.log(`The db contains ${db.length} objects`);
-    
+ 
+
     for(let i = 0; i < db.length; i++){
         console.log(db[i].id);
         if(db[i].id == req.params.id){
@@ -32,9 +33,20 @@ app.get('/api/developers/:id', (req, res) => {
         }
        
     }
+   
     res.status(404).end();
+});
+
+app.get('/api/developers/:id/:name/:address', (req, res) => {
+    const id = req.params.id;
+    const name = req.params.name;
+    const address = req.params.address;
+    console.log(id, name, address);
+    console.log(req.params.id);
 })
 const port = 3000;
+
+
 
 app.listen(port, () => {
     console.log(`app is listening to the port ${port}`);
